13e467520eca33dae41917ca14409ea30c8314a0
CpuKnows <jamaxwell1990@gmail.com>
2017-08-28 11:15:32 -0500

    Datepicker and code cleanup
    Added datepicker module
    Added backend code for querying SQL DB with date parameters
    Cleaned up code style

--- public/javascripts/create_chart.js
+++ public/javascripts/create_chart.js
@@ -1,116 +1,148 @@
-function dashboard(id, fData) {
+/**
+ * Builds the dashboard. Currently only the histogram
+ * @param id - id of html element to attach to
+ * @param fData - data to build dashboard from
+ */
+function dashboard(id, fData, varName) {
 
-    function histogram(fd){
-        var hg={}, hgDim={t: 60, b: 30, l: 0, r: 0};
+    function histogram(fData, varName){
+        var hgDim={t: 60, b: 30, l: 0, r: 0};
         hgDim.w = 500 - hgDim.l - hgDim.r;
         hgDim.h = 300 - hgDim.t - hgDim.b;
 
+        // title of histogram
+        d3.select(id).append('h1')
+            .html('Histogram of ' + varName);
+
         // create svg for histogram
-        var hgSvg = d3.select(id).append("svg")
-            .attr("width", hgDim.w + hgDim.l + hgDim.r)
-            .attr("height", hgDim.h + hgDim.t + hgDim.b)
-            .append("g")
-            .attr("transform", "translate(" + hgDim.l + "," + hgDim.t + ")");
+        var hgSvg = d3.select(id).append('svg')
+            .attr('width', hgDim.w + hgDim.l + hgDim.r)
+            .attr('height', hgDim.h + hgDim.t + hgDim.b)
+            .append('g')
+            .attr('transform', 'translate(' + hgDim.l + ',' + hgDim.t + ')');
 
         // create function for x-axis mapping
         var x = d3.scaleBand().rangeRound([0, hgDim.w])
-            .domain(fd.map(function(d) { return d[0]; }));
+            .domain(fData.map(function(d) { return d[0]; }));
 
         // add x-axis to histogram
-        hgSvg.append("g").attr("class", "x axis")
-            .attr("transform", "translate(0," + hgDim.h + ")")
+        hgSvg.append('g').attr('class', 'x axis')
+            .attr('transform', 'translate(0,' + hgDim.h + ')')
             .call(d3.axisBottom(x));
 
         // create function for y-axis mapping
         var y = d3.scaleLinear().range([hgDim.h - 20, 0])
-            .domain([0, d3.max(fd, function(d) { return d[1]; })]);
+            .domain([0, d3.max(fData, function(d) { return d[1]; })]);
 
-        var bars = hgSvg.selectAll(".bar").data(fd).enter()
-            .append("g").attr("class", "bar");
+        // data join to create bar elements
+        var bars = hgSvg.selectAll('.bar').data(fData).enter()
+            .append('g').attr('class', 'bar');
 
         // create rectangles
-        bars.append("rect")
-            .attr("x", function(d) { return x(d[0]); })
-            .attr("y", function(d) { return y(d[1]); })
-            .attr("width", x.bandwidth())
-            .attr("height", function(d) { return hgDim.h - y(d[1]); })
-            .attr("fill", "steelblue");
+        bars.append('rect')
+            .attr('x', function(d) { return x(d[0]); })
+            .attr('y', function(d) { return y(d[1]); })
+            .attr('width', x.bandwidth())
+            .attr('height', function(d) { return hgDim.h - y(d[1]); })
+            .attr('fill', 'steelblue');
 
         // create frequency labels above bars
-        bars.append("text").text(function(d) { return d3.format(",")(d[1]) })
-            .attr("x", function(d) { return x(d[0]) + x.bandwidth()/2; })
-            .attr("y", function(d) { return y(d[1]) - 5; })
-            .attr("text-anchor", "middle");
+        bars.append('text').text(function(d) { return d3.format(',')(d[1]) })
+            .attr('x', function(d) { return x(d[0]) + x.bandwidth()/2; })
+            .attr('y', function(d) { return y(d[1]) - 5; })
+            .attr('text-anchor', 'middle');
     }
 
-    var hg = histogram(fData);
+    histogram(fData, varName);
 }
 
-function updateDashboard(id, fData) {
-    var hg={}, hgDim={t: 60, b: 30, l: 0, r: 0};
+/**
+ * Updates the data in the histogram after an event.
+ * @param id - id of html element to attach to
+ * @param fData - data to build dashboard from
+ */
+function updateHistogram(id, fData, varName) {
+    var hgDim={t: 60, b: 30, l: 0, r: 0};
     hgDim.w = 500 - hgDim.l - hgDim.r;
     hgDim.h = 300 - hgDim.t - hgDim.b;
 
-    // create svg for histogram
-    var hgSvg = d3.select(id).select("svg").select("g");
+    // title of histogram
+    d3.select(id).select('h1')
+        .html('Histogram of ' + varName);
+
+    // select svg of histogram
+    var hgSvg = d3.select(id).select('svg').select('g');
 
     // create function for x-axis mapping
     var x = d3.scaleBand().rangeRound([0, hgDim.w])
         .domain(fData.map(function(d) { return d[0]; }));
 
     // add x-axis to histogram
-    hgSvg.select("g").call(d3.axisBottom(x));
+    hgSvg.select('g').call(d3.axisBottom(x));
 
     // create function for y-axis mapping
     var y = d3.scaleLinear().range([hgDim.h - 20, 0])
         .domain([0, d3.max(fData, function(d) { return d[1]; })]);
 
     // remove old bars
-    var bars = hgSvg.selectAll(".bar").data(fData, function(d) { return d; });
+    var bars = hgSvg.selectAll('.bar').data(fData, function(d) { return d; });
     bars.exit().remove();
 
     // add new bars
-    bars = hgSvg.selectAll(".bar").data(fData).enter()
-        .append("g").attr("class", "bar");
+    bars = hgSvg.selectAll('.bar').data(fData).enter()
+        .append('g').attr('class', 'bar');
 
     // create rectangles
-    bars.append("rect")
-        .attr("x", function(d) { return x(d[0]); })
-        .attr("y", function(d) { return y(d[1]); })
-        .attr("width", x.bandwidth())
-        .attr("height", function(d) { return hgDim.h - y(d[1]); })
-        .attr("fill", "steelblue");
+    bars.append('rect')
+        .attr('x', function(d) { return x(d[0]); })
+        .attr('y', function(d) { return y(d[1]); })
+        .attr('width', x.bandwidth())
+        .attr('height', function(d) { return hgDim.h - y(d[1]); })
+        .attr('fill', 'steelblue');
 
     // create frequency labels above bars
-    bars.append("text").text(function(d) { return d3.format(",")(d[1]) })
-        .attr("x", function(d) { return x(d[0]) + x.bandwidth()/2; })
-        .attr("y", function(d) { return y(d[1]) - 5; })
-        .attr("text-anchor", "middle");
+    bars.append('text').text(function(d) { return d3.format(',')(d[1]) })
+        .attr('x', function(d) { return x(d[0]) + x.bandwidth()/2; })
+        .attr('y', function(d) { return y(d[1]) - 5; })
+        .attr('text-anchor', 'middle');
 }
 
+// Builds initial dashboard on page load
 d3.json('/api/histogram?variable=id', function(err, res) {
-    sF = res.map(function(d) {return [d.id, d.freq]});
-    dashboard('#dashboard', sF);
+    fData = res.map(function(d) {return [d.id, d.freq]});
+    dashboard('#dashboard', fData, 'ID');
 });
 
-// event listeners
+
+/**
+ * Event listeners
+ */
+
+// Histogram button event listeners
 $('#graph-id').click(function() {
     d3.json('/api/histogram?variable=id', function(err, res) {
-        sF = res.map(function(d) {return [d.id, d.freq]});
-        updateDashboard('#dashboard', sF);
+        fData = res.map(function(d) {return [d.id, d.freq]});
+        updateHistogram('#dashboard', fData, 'ID');
     });
 });
 
 $('#graph-platform').click(function() {
     d3.json('/api/histogram?variable=platform', function(err, res) {
-        sF = res.map(function(d) {return [d.platform, d.freq]});
-        updateDashboard('#dashboard', sF);
+        fData = res.map(function(d) {return [d.platform, d.freq]});
+        updateHistogram('#dashboard', fData, 'Platform');
     });
 });
 
 $('#graph-action').click(function() {
     d3.json('/api/histogram?variable=action', function(err, res) {
-        sF = res.map(function(d) {return [d.action, d.freq]});
-        updateDashboard('#dashboard', sF);
+        fData = res.map(function(d) {return [d.action, d.freq]});
+        updateHistogram('#dashboard', fData, 'Action');
     });
+});
+
+// datapicker event listener
+$('.input-daterange').datepicker({
+    format: 'yyyy-mm-dd',
+    startDate: '2017-01-01',
+    endDate: '2019-12-31'
 });
--- routes/api/histogram.js
+++ routes/api/histogram.js
@@ -8,9 +8,17 @@ var db = new sqlite3.Database(path.join(__dirname, '../../data/activity.db'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    db.serialize(function () {
+    var apiString = '';
 
-        db.all('SELECT ' + req.query.variable + ', count(*) AS freq FROM activity GROUP BY ' + req.query.variable, [], function(err, rows) {
+    if(req.query.start !== undefined && req.query.end !== undefined) {
+        apiString = 'SELECT ' + req.query.variable + ', count(*) AS freq FROM activity GROUP BY ' + req.query.variable +
+            'WHERE datetime BETWEEN DATE(' + req.query.start + ') AND DATE(' + req.query.end + ')';
+    } else {
+        apiString = 'SELECT ' + req.query.variable + ', count(*) AS freq FROM activity GROUP BY ' + req.query.variable;
+    }
+
+    db.serialize(function () {
+        db.all(apiString, [], function(err, rows) {
             //db.close();
             res.json(rows);
         });
